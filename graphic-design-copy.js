81ab9da64a87b79e0dbd2ced3cd202a7dcbfa4d9
NiaBoonka <pioroangelika@gmail.com>
2025-05-10 21:21:13 +0200

    Update graphic-design-copy.js

--- graphic-design-copy.js
+++ graphic-design-copy.js
@@ -10,7 +10,7 @@ const projects = [
 
     {
         title: "New",
-        image: "images/packaging/wands.jpg", // Original image path
+        image: "images/packaging/wands.png", // Original image path
         size: "half",
         link: "https://google.com",
         
