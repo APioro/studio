b6708010b036bf99db99995c8211a972de14550b
NiaBoonka <pioroangelika@gmail.com>
2025-07-06 12:57:46 +0200

    hbgvujvu

--- packaging/packaging.js
+++ packaging/packaging.js
@@ -52,5 +52,26 @@ function createTiles() {
     });
 }
 
+function createObservers(){
+     document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    document.querySelectorAll('.fade-in-up').forEach((el) => {
+        observer.observe(el);
+    });
+    });
+}
+
 // Call the function to create tiles
 createTiles();
+createObservers();
+
