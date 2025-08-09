89c005af7dcb02fd1be475ac7da7c50022fb956b
NiaBoonka <pioroangelika@gmail.com>
2025-08-09 23:57:01 +0200

    delete blank (no new tab opening)

--- homepage-projects.js
+++ homepage-projects.js
@@ -62,7 +62,7 @@ function createTiles() {
 
     const link = document.createElement("a");
     link.href = project.link;
-    link.target = "_blank";
+   
     link.appendChild(img);
 
     // Create overlay
